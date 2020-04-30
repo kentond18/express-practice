01af5a6a6e8e24f79bfc4de47b76418079ebda41
Kenton Duprey <kdtech18@gmail.com>
2020-04-30 22:23:32 +0100

    adding middleware-practice

--- express-practices/hello-world/index.js
+++ express-practices/hello-world/index.js
@@ -15,6 +15,19 @@ app.all('/test', (req, res) => {
     res.send("HTTP method doesnt have any effect on this route!");
 });
 
-app.use('/things', things);
+app.get('/:id', (req, res) => {
+    res.send('The ID you specified is ' + req.params.id);
+});
+
+app.get('*', (req, res) => {
+    res.send('Sorry, this is an invalid URL.');
+});
+
+// app.use('/things' || '/things/', (req, res, next) => {
+//     console.log("A request for things received at " + Date.now());
+//     next();
+// });
+
+app.use('/things' || '/things/', things);
 
 app.listen(3000);
--- express-practices/hello-world/things.js
+++ express-practices/hello-world/things.js
@@ -1,11 +1,24 @@
 var express = require('express');
 var router = express.Router();
 
+router.use('/', (req, res, next) => {
+    console.log("A request for things received at " + Date.now());
+    next();
+});
+
 router.get('/', (req, res) => {
     res.send('GET route on things.');
 });
 router.post('/', (req, res) => {
     res.send('POST route on things.');
 });
+
+router.get('/:name/:id', (req, res) => {
+    res.send('id: ' + req.params.id + ' and name: ' + req.params.name);
+});
+
+router.get('/:id([0-9]{5})', (req, res) => {
+    res.send('id: ' + req.params.id);
+})
 
 module.exports = router;
